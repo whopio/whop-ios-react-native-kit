d12c53b3dd30d5bb103025e09a1359d883da02ee
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-06-17 10:36:44 -0700

    Release 0.0.1 - update binaryTarget URL & checksum

--- Package.swift
+++ Package.swift
@@ -21,12 +21,12 @@ let package = Package(
     .binaryTarget(
       name: "WhopReactNativeKit-Debug",
       url:
-        "https://github.com/whopio/whop-ios-react-native-kit/releases/download/$version/WhopReactNativeKit-Release.xcframework.zip",
-      checksum: "ff2342fe234fdaf2ea34fd2a5ef123da4f2ea43df23ea"),
+        "https://github.com/whopio/whop-ios-react-native-kit/releases/download/0.0.1/WhopReactNativeKit-Debug.xcframework.zip",
+      checksum: "8d7d5e5510e58af3df201c5cbb97864924db7e60614f2163aa3650018148de98"),
     .binaryTarget(
       name: "WhopReactNativeKit-Release",
       url:
-        "https://github.com/whopio/whop-ios-react-native-kit/releases/download/$version/WhopReactNativeKit-Release.xcframework.zip",
-      checksum: "helloworld"),
+        "https://github.com/whopio/whop-ios-react-native-kit/releases/download/0.0.1/WhopReactNativeKit-Release.xcframework.zip",
+      checksum: "4b8e3638f0f4361aa5ba75aef496920c371f0777fc682e85698eea58231619b3"),
   ]
 )
