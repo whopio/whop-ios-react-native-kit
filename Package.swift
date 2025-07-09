795beab27044fc4b43895d3e7e6411bcd85a27d7
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-07-09 14:44:28 -0400

    push

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   products: [
     .library(
       name: "WhopReactNativeKit",
-      targets: ["WhopReactNativeKit-Release"]
+      targets: ["WhopReactNativeKit"]
     ),
     .library(
       name: "hermes",
@@ -24,7 +24,7 @@ let package = Package(
         "https://github.com/whopio/whop-ios-react-native-kit/releases/download/0.0.15/hermes.xcframework.zip",
       checksum: "76a12f7fc47a53a68fc691c79a3855b2a95c2aadea2c26e0b92c558039263bde"),
     .binaryTarget(
-      name: "WhopReactNativeKit-Release",
+      name: "WhopReactNativeKit",
       url:
         "https://github.com/whopio/whop-ios-react-native-kit/releases/download/0.0.15/WhopReactNativeKit-Release.xcframework.zip",
       checksum: "9269d45e3d411f15f061f177ffeb644db8e427a95c66d78ba32e2f1daf0f7927"),
