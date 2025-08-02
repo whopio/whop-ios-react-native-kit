d877cb8a27604d35e410c77a6ba60f63843ac91f
Jordan Jantschulev <j.jantschulev@gmail.com>
2025-08-02 17:16:38 -0400

    Publish Android SDK 0.0.1

--- publish-android.sh
+++ publish-android.sh
@@ -4,15 +4,16 @@ set -e
 
 export PUBLISHING_VERSION=$(cat ./android_version.txt)
 
-mkdir -p ./ghpages/com/whoprnkit
+rm -rf ./docs/com/whoprnkit
+mkdir -p ./docs/com/whoprnkit
 
 # expect there to be a file in the ~/.m2/repository...
 ls -la ~/.m2/repository/com/whoprnkit/whopreactnativekit/$PUBLISHING_VERSION/whopreactnativekit-$PUBLISHING_VERSION-release.aar
 ls -la ~/.m2/repository/com/whoprnkit/whopreactnativekit/$PUBLISHING_VERSION/whopreactnativekit-$PUBLISHING_VERSION-debug.aar
 
-cp -r ~/.m2/repository/com/whoprnkit/whopreactnativekit ./ghpages/com/whoprnkit/whopreactnativekit
+cp -r ~/.m2/repository/com/whoprnkit/whopreactnativekit ./docs/com/whoprnkit/whopreactnativekit
 
-git add ./ghpages
+git add ./docs
 git commit -m "Publish Android SDK $PUBLISHING_VERSION"
 git push
 
