d7650bd4b1fb643d6158fa3ae0b9c2fc8295cc3d
Aaron <aaron@unionst.com>
2025-11-03 10:43:27 -0800

    Release 0.1.0 - update binaryTarget URL & checksum

--- Package.swift
+++ Package.swift
@@ -21,12 +21,12 @@ let package = Package(
     .binaryTarget(
       name: "hermes",
       url:
-        "https://github.com/whopio/whop-ios-react-native-kit/releases/download/0.0.32/hermes.xcframework.zip",
-      checksum: "76a12f7fc47a53a68fc691c79a3855b2a95c2aadea2c26e0b92c558039263bde"),
+        "https://github.com/whopio/whop-ios-react-native-kit/releases/download/0.1.0/hermes.xcframework.zip",
+      checksum: "3e49f30c289cae9cad53733e7f21c42c39802e05e506de263c4d77e947b5c739"),
     .binaryTarget(
       name: "WhopReactNativeKit",
       url:
-        "https://github.com/whopio/whop-ios-react-native-kit/releases/download/0.0.32/WhopReactNativeKit-Release.xcframework.zip",
-      checksum: "588848426d6bd63beef66b232487d9242b2ec359878f3d116f83f0b1130265c7"),
+        "https://github.com/whopio/whop-ios-react-native-kit/releases/download/0.1.0/WhopReactNativeKit-Release.xcframework.zip",
+      checksum: "e7697da978abb6b9cd7efbd16bcb77f0c4ffb1617138269908c529d7c443128b"),
   ]
 )
