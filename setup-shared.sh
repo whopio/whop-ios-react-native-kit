1ec60b857247db64e9e3d77f532ccf9fadd242f2
Aaron <aaron@unionst.com>
2025-11-04 14:51:57 -0800

    Upgrade RN version to 0.81.5

--- setup-shared.sh
+++ setup-shared.sh
@@ -7,7 +7,7 @@ root=$(pwd)
 rm -rf ./react-native
 mkdir -p ./react-native
 cd ./react-native
-npx @react-native-community/cli@19.0.0 init WhopReactNativeKit --version 0.80.0 --skip-git-init --install-pods false
+npx @react-native-community/cli@20.0.0 init WhopReactNativeKit --version 0.81.5 --skip-git-init --install-pods false
 cd ./WhopReactNativeKit
 npm install \
   react-native-nitro-modules@0.26.3 \
@@ -15,8 +15,8 @@ npm install \
   @d11/react-native-fast-image@8.10.0 \
   react-native-svg@15.12.0 \
   react-native-webview@13.15.0 \
-  react-native-reanimated@3.18.0 \
-  react-native-gesture-handler@2.27.2 \
+  react-native-reanimated@3.19.0 \
+  react-native-gesture-handler@2.29.1 \
   react-native-haptic-feedback@2.3.3 \
   react-native-vision-camera@4.7.1 \
   react-native-safe-area-context@5.5.2 \
